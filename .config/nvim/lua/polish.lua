c66bd7171af8e51ada578ae481189e3e10876bbb
Lewis Blackburn <lewis.blackburn@ballys.com>
2025-11-27 22:02:49 +0000

    git message prefixes

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -1,5 +1,4 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+vim.env.LG_CONFIG_FILE = vim.fn.expand "~/.config/lazygit/config.yml"
