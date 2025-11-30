7ffab5399083a1a49a33b4a335177969224c0f81
Lewis Blackburn <lewis.blackburn@ballys.com>
2025-11-30 19:52:51 +0000

    fix formatter jump

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -48,6 +48,7 @@ return {
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
+        autoformat_enabled = false,
       },
     },
     -- Mappings can be configured through AstroCore as well.
