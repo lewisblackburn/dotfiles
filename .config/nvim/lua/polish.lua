724d83118c3e9e0efe26da6c176e68e9cea46a5d
Lewis Blackburn <lewis.blackburn@ballys.com>
2026-02-08 15:36:27 +0000

    nvcahd

--- .config/nvim/lua/polish.lua
+++
@@ -1,4 +0,0 @@
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
-vim.env.LG_CONFIG_FILE = vim.fn.expand "~/.config/lazygit/config.yml"
